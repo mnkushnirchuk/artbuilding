165336fb79ad2b95c235b703daccb9e616685ebd
Marharyta Kushnirchuk <rita.kushnirchuk@gmail.com>
2024-02-27 21:40:46 +0300

    feat: work with main page

--- src/js/app.js
+++ src/js/app.js
@@ -179,19 +179,36 @@ BaseHelpers.addLoadedClass();
 
 
 
+
+// setTimeout(() => {
+// 	document.querySelector('.preloader').style.display = 'none';
+// 	document.querySelector('.slider').style.display = 'flex';
+// 	const elementsToAnimate = document.querySelectorAll('.slider__information, .information__counter');
+
+// 	elementsToAnimate.forEach((element, index) => {
+// 		setTimeout(() => {
+// 			element.classList.add('animate');
+// 		}, index * 1000);
+// 	});
+// 	if(currentSlideIndex == 1){
+	
+// 	}
+// }, 2000);
+
 document.querySelector('.preloader').style.display = 'flex';
 document.querySelector('.slider').style.display = 'none';
-
 setTimeout(() => {
-	document.querySelector('.preloader').style.display = 'none';
-	document.querySelector('.slider').style.display = 'flex';
-	const elementsToAnimate = document.querySelectorAll('.slider__information, .information__counter');
+    document.querySelector('.preloader').style.display = 'none';
+    document.querySelector('.slider').style.display = 'flex';
 
-	elementsToAnimate.forEach((element, index) => {
+		const specialElements = document.querySelectorAll('.slider__information, .information__counter, .block__title, .block__text, .information__more');
+		specialElements.forEach((element, index) => {
 		setTimeout(() => {
 			element.classList.add('animate');
 		}, index * 1000);
 	});
+
+    changeSlide(1);
 }, 2000);
 
 const informationList = document.querySelector('.information-list');
@@ -211,34 +228,37 @@ const currentNum = document.querySelector('.current-num');
 const prevArrow = document.querySelector('.arrow-prev');
 const nextArrow = document.querySelector('.arrow-next');
 let currentSlideIndex = 1;
-let autoSlideTimer; // Объявляем переменную для таймера
+let autoSlideTimer; 
 
-// Функция для смены слайда
 function changeSlide(slideNumber) {
 	const elementsToAnimate = document.querySelectorAll('.block__title, .block__text, .information__more');
-
 	elementsToAnimate.forEach((element, index) => {
 		element.classList.remove('animate');
 	});
 	document.querySelectorAll('.information__block').forEach(block => {
-		// block.style.display = 'none';
 		block.classList.remove('active');
 	});
 
-	// document.querySelector('.information__block-' + slideNumber).style.display = 'block';
+
 	const activeSlide = document.querySelector('.information__block-' + slideNumber);
 	activeSlide.classList.add('active');
 	const blockAnimateElements = activeSlide.querySelectorAll('.block__title, .block__text, .information__more');
+	const specialElements = activeSlide.querySelectorAll('.slider__information, .information__counter, .block__title, .block__text, .information__more');
 	blockAnimateElements.forEach((element, index) => {
 		setTimeout(() => {
 			element.classList.add('animate');
 		}, index * 1000);
 	});
+	if(currentSlideIndex === 1){
+		specialElements.forEach((element, index) =>{
+			setTimeout(()=>{
+				element.classList.add('animate');
+			}, index * 1000)
+		})
+	};
 	document.querySelectorAll('.pagination__item').forEach(item => {
 		item.classList.remove('pagination__item--active');
 	});
-	// Необходимо убедиться, что эта строка корректно вписывается в ваш контекст, так как она отсутствовала в изначальном коде
-	// informationList.style.display = 'none'; 
 	document.querySelector('.information__burger-menu').classList.remove('active');
 
 	document.querySelectorAll('.pagination__item__wrapper')[slideNumber - 1].children[0].classList.add('pagination__item--active');
@@ -252,7 +272,6 @@ function changeSlide(slideNumber) {
 		}
 	});
 
-	// Перезапуск таймера авто-переключения после ручного переключения слайда
 	resetAutoSlideTimer();
 }
 
