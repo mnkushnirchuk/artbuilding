934c45960185d874ccaee52e0741d27dd662432d
Marharyta Kushnirchuk <rita.kushnirchuk@gmail.com>
2024-03-01 14:13:26 +0300

    feat: fix all bugs and add adaptability for all blocks

--- src/js/pages/aboutTeam.js
+++ src/js/pages/aboutTeam.js
@@ -1,16 +1,21 @@
 import Swiper, { Navigation } from 'swiper';
 
 const aboutTeam = (slider) => {
-  new Swiper(slider, {
+  const swiper = new Swiper(slider, {
     speed: 800,
     slidesPerView: 'auto',
     modules: [Navigation],
     loop: true,
-    // centeredSlides: true,
     navigation: {
       prevEl: ".swiper-button-prev",
       nextEl: ".swiper-button-next"
     },
+    on: {
+      slideChange: function () {
+        const currentIndex = this.realIndex + 1; 
+        document.querySelector('.about__team-counter .current').textContent = currentIndex.toString().padStart(2, '0');
+      }
+    }
   });
 }
 export default aboutTeam;
