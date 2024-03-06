d0dad4b6fc7db132bd1f12651e5f6302d9689b99
Marharyta Kushnirchuk <rita.kushnirchuk@gmail.com>
2024-03-06 13:13:30 +0300

    feat: update js basic helpers

--- src/js/helpers/BaseHelpers.js
+++ src/js/helpers/BaseHelpers.js
@@ -19,13 +19,13 @@ class BaseHelpers {
         'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
     };
 
-    /** Добавление класса _webp или _no-webp для HTML */
-    testWebp((support) => {
-      const className = support ? 'webp' : 'no-webp';
-      this.html.classList.add(className);
+    // /** Добавление класса _webp или _no-webp для HTML */
+    // testWebp((support) => {
+    //   const className = support ? 'webp' : 'no-webp';
+    //   this.html.classList.add(className);
 
-      console.log(support ? 'webp поддерживается' : 'webp не поддерживается');
-    });
+    //   console.log(support ? 'webp поддерживается' : 'webp не поддерживается');
+    // });
   }
 
   /**
